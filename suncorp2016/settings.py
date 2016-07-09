63823a35a91a2f4a5bbff13e8290f3b8d227169e
Zerong Tony Wang <ztony_wang@hotmail.com>
2016-07-09 20:11:28 +1000

    added auth

--- suncorp2016/settings.py
+++ suncorp2016/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'suncorp2016.urls'
